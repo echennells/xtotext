3c7ce33813a9133807b85cee9dfe0f1b0b968518
Eric Chennells <eric@brodie.rocks>
2025-07-27 11:04:59 -0700

    post cleanup

--- data/one_off_transcripts/extract_text.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open("AMA： This Week's Guest Is YOU! ｜ Ep 29 ｜ S5_UTsBQ-aVx1s_transcript.json", "r") as f:
-    data = json.load(f)
-    
-with open("AMA_clean_transcript.txt", "w") as f:
-    f.write(data["text"])
